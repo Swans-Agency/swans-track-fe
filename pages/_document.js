e0cb77118824693be7979fc62e99137f06c61051
yazanismail1 <yalfarra@outlook.com>
2023-07-15 11:51:24 +0000

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-EQF1MDFZBL"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '{{ GA_TRACKING_ID }}');
+          `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
