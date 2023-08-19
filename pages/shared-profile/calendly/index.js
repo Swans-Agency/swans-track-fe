273fc6c1e7e875e9beb536876e809ef49ffdb588
omarali1997 <omar.aziz.9718@gmail.com>
2023-08-19 19:48:43 +0000

    design Booking

--- pages/shared-profile/calendly/index.js
+++ pages/shared-profile/calendly/index.js
@@ -1,239 +1,31 @@
 import React from "react";
 import Navbar from "@/components/Calendly/Navbar";
 import CalendlyForm from "@/components/Calendly/CalendlyForm";
-import { Table } from "antd";
-import { RightOutlined, LeftOutlined } from "@ant-design/icons";
+import Calender from "@/components/Calendly/Calender";
+import Image from "next/image";
 
 export default function Index(props) {
-  const dataSource = [
-    {
-      key: "1",
-      SU: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>1</h1>
-        </div>
-      ),
-      MO: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>2</h1>
-        </div>
-      ),
-      TU: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>3</h1>
-        </div>
-      ),
-      WE: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>4</h1>
-        </div>
-      ),
-      TH: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>5</h1>
-        </div>
-      ),
-      FR: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>6</h1>
-        </div>
-      ),
-      ST: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>7</h1>
-        </div>
-      ),
-    },
-    {
-      key: "2",
-      SU: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>8</h1>
-        </div>
-      ),
-      MO: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>9</h1>
-        </div>
-      ),
-      TU: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>10</h1>
-        </div>
-      ),
-      WE: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>11</h1>
-        </div>
-      ),
-      TH: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>12</h1>
-        </div>
-      ),
-      FR: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>13</h1>
-        </div>
-      ),
-      ST: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>14</h1>
-        </div>
-      ),
-    },
-    {
-      key: "3",
-      SU: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>15</h1>
-        </div>
-      ),
-      MO: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>16</h1>
-        </div>
-      ),
-      TU: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>17</h1>
-        </div>
-      ),
-      WE: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>18</h1>
-        </div>
-      ),
-      TH: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>19</h1>
-        </div>
-      ),
-      FR: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>20</h1>
-        </div>
-      ),
-      ST: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>21</h1>
-        </div>
-      ),
-    },
-    {
-      key: "4",
-      SU: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>22</h1>
-        </div>
-      ),
-      MO: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>23</h1>
-        </div>
-      ),
-      TU: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>24</h1>
-        </div>
-      ),
-      WE: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>25</h1>
-        </div>
-      ),
-      TH: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>26</h1>
-        </div>
-      ),
-      FR: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>27</h1>
-        </div>
-      ),
-      ST: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>28</h1>
-        </div>
-      ),
-    },
-    {
-      key: "5",
-      SU: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>29</h1>
-        </div>
-      ),
-      MO: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>30</h1>
-        </div>
-      ),
-      TU: (
-        <div className="border-2 text-center border-black px-3 py-2  rounded-full hover:text-blue-400 hover:border-blue-400 ">
-          <h1>31</h1>
-        </div>
-      ),
-    },
-  ];
-
-  const columns = [
-    {
-      title: "SU",
-      dataIndex: "SU",
-      key: "SU",
-    },
-    {
-      title: "MO",
-      dataIndex: "MO",
-      key: "MO",
-    },
-    {
-      title: "TU",
-      dataIndex: "TU",
-      key: "TU",
-    },
-    {
-      title: "WE",
-      dataIndex: "WE",
-      key: "WE",
-    },
-    {
-      title: "TH",
-      dataIndex: "TH",
-      key: "TH",
-    },
-    {
-      title: "FR",
-      dataIndex: "FR",
-      key: "FR",
-    },
-    {
-      title: "ST",
-      dataIndex: "ST",
-      key: "ST",
-    },
-  ];
-
   return (
     <section>
       <Navbar />
       <div className="w-[100%] items-center content-center mt-10">
         <div className="w-[90%] m-auto flex justify-center gap-20">
           <CalendlyForm />
-          <div>
-            <div className="flex gap-2">
-              <h1 className="pb-10 ps-5 font-semibold -tracking-tighter">
-                August
-              </h1>
-              <LeftOutlined />
-              <RightOutlined />
-            </div>
-            <Table dataSource={dataSource} columns={columns} />;
-          </div>
+          <Calender />
         </div>
       </div>
+
+      <footer className="bg-black">
+      <div className="text-center px-[10%] bg-sidebar text-white">
+        <h1>
+          2023 @ Swans Track LTD, all right reseved
+        </h1>
+        <a href="#" className="text-blue-400">
+          Terms of use | privacy Policy
+        </a>
+        
+      </div>
+      </footer>
     </section>
   );
 }
