44c450392940af93693e2a53c513407ee4bfc07c
omarali1997 <omar.aziz.9718@gmail.com>
2023-09-29 22:38:08 +0000

    responsive Landing page

--- components/LandingPage/Footer.js
+++ components/LandingPage/Footer.js
@@ -5,36 +5,44 @@ import Link from "next/link";
 
 export default function Footer(props) {
   return (
-    <section className="bg-gradient-to-b from-[#01417E] to-[#019DF7] h-[25vh] grid">
-      <div className="w-[80%] m-auto flex justify-between ">
-        <div className="flex gap-24">
+    <section className="bg-gradient-to-b from-[#01417E] to-[#019DF7] py-10">
+      <div className="w-[90%] m-auto desktop:flex desktop:flex-wrap desktop:justify-between phone:">
+        <div className=" desktop:flex gap-24">
 
-        <div className="self-center">
-          <Image src="/logoNew.svg" width={100} height={100} />
+        <div className="self-center phone:flex phone:justify-center phone:mb-5">
+          <Image src="/LogoWhite.svg" width={100} height={100} />
         </div>
-        <div>
-          <h1 className="text-white text-[1.375rem]">Stay Connected</h1>
-          <div className="space-x-2 pt-2">
-            <Input
-              placeholder="Type your email"
-              className="w-[356px] h-[48px] rounded-lg"
-            />
-            <button className="border w-[117px] h-[48px] rounded-lg bg-white text-[#0191E7] font-bold text-[1.125rem]">
-              Submit
-            </button>
-          </div>
-        </div>
-        </div>
-        <div className="flex flex-col gap-10 pt-4 ">
-          <div className="flex justify-between gap-10">
+        <div className="phone:flex justify-between gap-1 desktop:hidden phone:mb-10">
             <button className="text-white">Home</button>
             <button className="text-white">Features</button>
             <button className="text-white">Users</button>
             <button className="text-white">Sign up</button>
           </div>
-          <div className="  space-x-3 text-white">
-            <Link href="/">© 2023 Swanstrack Ltd</Link>
-            <span>|</span>
+        <div>
+          <h1 className="text-white text-[1.375rem] phone:text-center desktop:text-start">Stay Connected</h1>
+          <div className="desktop:flex gap-2 pt-2 phone:place-items-center phone:grid">
+            <Input
+              placeholder="Type your email"
+              className="max-w-[356px] desktop:w-[356px] h-[48px] rounded-lg phone:items-center desktop:m-0"
+            />
+            <div className="flex justify-center phone:pt-3 desktop:pt-0">
+            <button className="border w-[117px] h-[48px] rounded-lg bg-white text-[#0191E7] font-bold text-[1.125rem]">
+              Submit
+            </button>
+            </div>
+          </div>
+        </div>
+        </div>
+        <div className="flex flex-col gap-10 pt-4 ">
+          <div className="desktop:flex justify-between gap-10 phone:hidden">
+            <button className="text-white">Home</button>
+            <button className="text-white">Features</button>
+            <button className="text-white">Users</button>
+            <button className="text-white">Sign up</button>
+          </div>
+          <div className="flex flex-wrap phone:justify-center gap-3 text-white phone:text-center w-full">
+            <p className="phone:text-center phone:block phone:w-[100%] desktop:w-fit">© 2023 Swanstrack Ltd</p>
+            <span className="desktop:block phone:hidden">|</span>
             <Link href="/terms-conditions">Terms</Link>
             <span>|</span>
             <Link href="/swans-cookies">Cookies</Link>
--- pages/login/index.js
+++ pages/login/index.js
@@ -20,82 +20,91 @@ export default function index() {
   return (
     <>
       <section className="w-full h-screen grid grid-cols-12">
-<div className="col-span-4 w-full h-screen" style={{backgroundImage:'url(/BgForms.svg)',backgroundPosition:'center' , backgroundSize:'cover'}}>
-
-        {/* <Image src="/BgForms.svg" width={1000} height={1000} className="h-screen w-[100%]"/> */}
-</div>
-
-<div className="col-span-8 w-[90%] m-auto">
-<h1 className="text-[#0191E7] text-[3.75rem] text-center">Sign In</h1>
-<Form
-          onFinish={onFinish}
-          layout="vertical"
-          size="large"
+        <div
+          className="col-span-4 w-full h-screen"
           style={{
-            alignContent: "center",
-            maxWidth: 700,
-            paddingRight: 25,
-            paddingLeft: 25,
+            backgroundImage: "url(/BgForms.svg)",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
           }}
         >
-          <Form.Item
-            label="E-mail"
-            name="username"
-            rules={[
-              {
-                type: "email",
-                message: "The input is not valid E-mail!",
-              },
-            ]}
-            required
-            tooltip="E-mail address used in the signing up process."
-          >
-            <Input className="rounded" placeholder="support@swanstrack.com" />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            required
-            name="password"
-            tooltip={{
-              title: "Your password.",
+          {/* <Image src="/BgForms.svg" width={1000} height={1000} className="h-screen w-[100%]"/> */}
+        </div>
+
+        <div className="col-span-8 w-[90%] m-auto">
+          <h1 className="text-[#0191E7] text-[3.75rem] text-center">Sign In</h1>
+          <Form
+            onFinish={onFinish}
+            layout="vertical"
+            size="large"
+            style={{
+              alignContent: "center",
+              maxWidth: 700,
+              paddingRight: 25,
+              paddingLeft: 25,
             }}
           >
-            <Input type="password" className="rounded" placeholder="password" />
-          </Form.Item>
-          <Form.Item>
-            <div className="relative">
-
-            <button
-              htmlType="submit"
-              size="large"
-              className="rounded w-[100%] bg-[#163757] hover:shadow-xl py-2 text-white flex gap-x-2 text-center justify-center"
+            <Form.Item
+              label="E-mail"
+              name="username"
+              rules={[
+                {
+                  type: "email",
+                  message: "The input is not valid E-mail!",
+                },
+              ]}
+              required
+              tooltip="E-mail address used in the signing up process."
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="white"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                />
-              </svg>
-              Login
-            </button>
-            <a
-              className="absolute left-1/2 -translate-x-1/2 mt-2 text-blue-600 text-center font-light text-sm hover:cursor-pointer hover:text-blue-500"
-              href="/forget-password"
+              <Input className="rounded" placeholder="support@swanstrack.com" />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              required
+              name="password"
+              tooltip={{
+                title: "Your password.",
+              }}
             >
-              Forget password!
-            </a>
-            </div>
-          </Form.Item>
-        </Form>
-</div>
+              <Input
+                type="password"
+                className="rounded"
+                placeholder="password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <div className="relative">
+                <button
+                  htmlType="submit"
+                  size="large"
+                  className="rounded w-[100%] bg-[#163757] hover:shadow-xl py-2 text-white flex gap-x-2 text-center justify-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="white"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                    />
+                  </svg>
+                  Login
+                </button>
+                <a
+                  className="absolute left-1/2 -translate-x-1/2 mt-2 text-blue-600 text-center font-light text-sm hover:cursor-pointer hover:text-blue-500"
+                  href="/forget-password"
+                >
+                  Forget password!
+                </a>
+              </div>
+            </Form.Item>
+          </Form>
+        </div>
       </section>
 
       {/* <div className="bg-cover bg-center min-h-screen bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-black via-gray-700 to-gray-500 flex flex-col items-center">
