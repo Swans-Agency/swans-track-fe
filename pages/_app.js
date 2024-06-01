2a4f802c50f4cc1c11488f34818147e7660640d8
yazanismail1 <yalfarra@outlook.com>
2024-06-01 10:01:58 +0000

    chore: Update _app.js to include Facebook Pixel tracking and handle route changes

--- pages/_app.js
+++ pages/_app.js
@@ -13,11 +13,18 @@ export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
     fbq.pageview();
+
     const handleRouteChange = () => {
       fbq.pageview();
     };
-  }, [router.events])
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
