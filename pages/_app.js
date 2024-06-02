56171b27e49a936bdfe3b2f9cbbd0578fdb8b65c
yazanismail1 <yalfarra@outlook.com>
2024-06-02 14:09:44 +0000

    chore: Update _app.js to include Facebook Pixel tracking and handle route changes

--- pages/_app.js
+++ pages/_app.js
@@ -31,12 +31,12 @@ export default function App({ Component, pageProps }) {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-
-      <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      <>
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -47,8 +47,9 @@ export default function App({ Component, pageProps }) {
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', ${fbq.FB_PIXEL_ID});
           `,
-        }}
-      />
+          }}
+        />
+      </>
 
       <AuthWrapper>
         <NavShowWraper>
